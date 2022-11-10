fab1f40de19f4d504ba5aaea8511cbb5f4e15b57
Herbie Vine <vineherbie@gmail.com>
2022-11-10 10:52:48 +0100

    finished stdout

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: herbie <herbie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 11:55:57 by herbie            #+#    #+#             */
-/*   Updated: 2022/11/09 14:39:49 by herbie           ###   ########.fr       */
+/*   Updated: 2022/11/10 10:06:58 by herbie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,5 +54,11 @@ int ft_isascii(int c);
 int ft_isprint(int c);
 int ft_tolower(int c);
 int ft_toupper(int c);
+
+// Stdout
+void ft_putchar_fd(char c, int fd);
+void ft_putstr_fd(char *s, int fd);
+void ft_putendl_fd(char *s, int fd);
+void ft_putnbr_fd(int n, int fd);
 
 #endif
