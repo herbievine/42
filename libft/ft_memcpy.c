f27f7e0b3032f496838899731304e6b40874740f
Herbie Vine <vineherbie@gmail.com>
2022-11-10 15:21:54 +0100

    made a small adjustment

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: herbie <herbie@student.42.fr>              +#+  +:+       +#+        */
+/*   By: hvine <hvine@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 12:28:28 by herbie            #+#    #+#             */
-/*   Updated: 2022/11/08 08:29:28 by herbie           ###   ########.fr       */
+/*   Updated: 2022/11/10 15:21:11 by hvine            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void *ft_memcpy(void *dest, const void *src, size_t n)
 {
 	if (!dest && !src)
 		return (0);
-	while (n-- > 0)
+	while (n--)
 		((unsigned char *)dest)[n] = ((char *)src)[n];
 
 	return dest;
