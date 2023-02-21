ec91f8a585f430d9c2a31646a2fcd72bf5563a6c
Herbie Vine <vineherbie@gmail.com>
2023-02-21 15:05:08 +0100

    force error

--- push_swap/parse.h
+++ push_swap/parse.h
@@ -6,13 +6,13 @@
 /*   By: herbie <herbie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 21:50:03 by herbie            #+#    #+#             */
-/*   Updated: 2023/02/21 15:04:44 by herbie           ###   ########.fr       */
+/*   Updated: 2023/02/21 15:04:59 by herbie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSE_H
 # define PARSE_H
 
-int	*ft_parse_args(int argc, char **argv);
+// int	*ft_parse_args(int argc, char **argv);
 
 #endif /* PARSE_H */
