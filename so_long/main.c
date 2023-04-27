1cc76078ade51ccf813b6b70c699a8bceb9a3f68
Herbie Vine <vineherbie@gmail.com>
2023-04-27 16:12:52 +0200

    compilation successful

--- so_long/main.c
+++ so_long/main.c
@@ -6,7 +6,7 @@
 /*   By: herbie <herbie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 16:00:58 by herbie            #+#    #+#             */
-/*   Updated: 2023/04/27 11:49:29 by herbie           ###   ########.fr       */
+/*   Updated: 2023/04/27 16:10:09 by herbie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ int main(void)
 	void *mlx;
 
 	mlx = mlx_init();
-	mlx_new_window(mlx, 500, 500, "mlx_project");
+	mlx_new_window(mlx, 500, 500, "herbievine: so_long");
 	mlx_loop(mlx);
 }
