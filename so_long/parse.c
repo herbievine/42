23f19084f5f7a06e5f1e7415e1e3da13bc90d172
Herbie Vine <vineherbie@gmail.com>
2023-05-07 12:52:39 +0200

    fix width x height + fix malloc issues

--- so_long/parse.c
+++ so_long/parse.c
@@ -26,6 +26,7 @@ t_bool	ft_is_map_possible(t_map *map, int px, int py, t_flood *flood);
 
 void	ft_parse_map_or_throw(t_map *map, char *map_path)
 {
+	t_flood	*flood;
 	char	*buffer;
 	int		fd;
 
@@ -38,9 +39,16 @@ void	ft_parse_map_or_throw(t_map *map, char *map_path)
 	if (!map->map)
 		ft_err(EMAP, map);
 	ft_get_map_info(map);
+	flood = (t_flood *)malloc(sizeof(t_flood));
+	if (!flood)
+		ft_err(EUNKN, map);
 	if (!ft_check_basic_rules(map)
-		|| !ft_is_map_possible(map, map->start.x, map->start.y, NULL))
+		|| !ft_is_map_possible(map, map->start.x, map->start.y, flood))
+	{
+		free(flood);
 		ft_err(EMAP, map);
+	}
+	free(flood);
 }
 
 void	ft_get_map_info(t_map *map)
@@ -61,9 +69,7 @@ void	ft_get_map_info(t_map *map)
 				|| (map->map[i][j] == COLL && ++map->collectibles))
 				;
 			if (map->map[i][j] == STRT)
-			{
 				map->start = (t_start){j, i};
-			}
 		}
 	}
 	map->height = i;
@@ -74,7 +80,8 @@ t_bool	ft_check_basic_rules(t_map *map)
 	int	i;
 	int	j;
 
-	if (map->collectibles < 1 || map->exits != 1 || map->entries != 1)
+	if (map->collectibles < 1 || map->exits != 1 || map->entries != 1
+		|| map->width == map->height)
 		return (false);
 	i = -1;
 	while (map->map[++i])
@@ -95,11 +102,8 @@ t_bool	ft_check_basic_rules(t_map *map)
 
 t_bool	ft_is_map_possible(t_map *map, int px, int py, t_flood *flood)
 {
-	if (!flood)
+	if (!flood->collectibles && !flood->exits)
 	{
-		flood = malloc(sizeof(t_flood));
-		if (!flood)
-			ft_err(EUNKN, map);
 		flood->collectibles = 0;
 		flood->exits = 0;
 	}
