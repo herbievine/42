f61a127fc0b849ac2258e409fccea2cc4cdf7992
Julien Cros <julien.cros86@gmail.com>
2023-11-17 15:58:36 +0100

    fix: throw error in in_fd

--- minishell/find_in_file.c
+++ minishell/find_in_file.c
@@ -6,7 +6,7 @@
 /*   By: juliencros <juliencros@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 15:02:31 by juliencros        #+#    #+#             */
-/*   Updated: 2023/11/17 15:18:23 by juliencros       ###   ########.fr       */
+/*   Updated: 2023/11/17 15:43:04 by juliencros       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,16 @@
 #include "error.h"
 #include "str.h"
 #include "parse.h"
+#include "display.h"
 #include <unistd.h>
 #include <fcntl.h>
+
+static void ft_print_error(char *error)
+{
+	ft_putstr_fd(M, STDERR_FILENO);
+	ft_putstr_fd(error, STDERR_FILENO);
+	ft_putstr_fd("\n", STDERR_FILENO);
+}
 
 void	ft_close_in_files(t_subcommand *subcommand)
 {
@@ -40,17 +48,17 @@ bool	ft_set_in_fd(t_subcommand *subcommand, t_token *token)
 		if (token->type == TOKEN_LT)
 		{
 			if (!token->next || ft_strlen(token->next->value) == 0)
-				return (printf(M""ESYN" `newline'\n"), false);
+				return (ft_print_error(ESYN), false);
 			if (token->next->type != TOKEN_SYMBOL && token->next->type != TOKEN_SQ
 				&& token->next->type != TOKEN_DQ)
-				return (printf(M""ESYN" `%s'\n", token->next->value), false);
+				return (ft_print_error(ESYN), false);
 			path = ft_substr(token->next->value, 0, token->next->length);
 			ft_close_in_files(subcommand);
 			fd = open(path, O_RDONLY);
 			if (fd == -1)
 			{
 				g_signal = 1;
-				return (printf(M"%s: "ENOENT"\n", path), free(path), false);
+				return (ft_print_error(ENOENT), free(path), false);
 				subcommand->is_executable = false;
 			}
 			free(path);
