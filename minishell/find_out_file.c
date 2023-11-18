05fd6c2a55a139bba0caec026c2bff9dbb528752
Julien Cros <julien.cros86@gmail.com>
2023-11-18 14:43:10 +0100

    fix: not closed out_fd + keep last ou_fd

--- minishell/find_out_file.c
+++ minishell/find_out_file.c
@@ -6,7 +6,7 @@
 /*   By: juliencros <juliencros@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 18:18:30 by juliencros        #+#    #+#             */
-/*   Updated: 2023/11/18 14:15:48 by juliencros       ###   ########.fr       */
+/*   Updated: 2023/11/18 14:41:45 by juliencros       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,10 @@ bool	ft_check_if_others_pipe(t_token *token);
 
 bool	ft_set_out_file(t_token *token, t_subcommand *subcommand)
 {
-	t_token	*first_token;
-
 	if (!token || token->type == TOKEN_PIPE
 		|| ft_check_if_others_pipe(token))
 		return (true);
-	first_token = token;
-	while (first_token->next != NULL && first_token->type != TOKEN_PIPE)
-	{
-		if (first_token->type == TOKEN_GT || first_token->type == TOKEN_GT_GT)
-			return (ft_set_out_fd(subcommand,
-					first_token, first_token->length), true);
-		first_token = first_token->next;
-	}
-	return (true);
+	return (ft_set_out_fd(subcommand, token));
 }
 
 bool	ft_check_if_others_pipe(t_token *token)
@@ -62,18 +52,20 @@ static void	ft_error_out_file(t_subcommand *subcommand,
 }
 
 bool	ft_set_out_fd(t_subcommand *subcommand,
-	t_token *token, int token_length)
+	t_token *token)
 {
 	char	*path;
 	int		fd;
 
-	while (token && token_length > 0)
+	while (token)
 	{
 		if (token->type == TOKEN_GT || token->type == TOKEN_GT_GT)
 		{
 			if (!token->next || token->next->type != TOKEN_SYMBOL)
 				return (ft_error_out_file(subcommand, token, ESYN), false);
 			path = ft_substr(token->next->value, 0, token->next->length);
+			if (subcommand->out_fd > 0)
+				close(subcommand->out_fd);
 			if (token->type == TOKEN_GT)
 				fd = open(path, O_RDWR | O_CREAT | O_TRUNC, 0644);
 			else
@@ -83,10 +75,9 @@ bool	ft_set_out_fd(t_subcommand *subcommand,
 			else
 				subcommand->out_file_name = ft_strdup(path);
 			free(path);
-			return (subcommand->out_fd = fd, true);
+			subcommand->out_fd = fd;
 		}
 		token = token->next;
-		token_length--;
 	}
 	return (true);
 }
