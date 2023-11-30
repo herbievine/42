0af5d4500eb0d57c95faec98e9a33a0650cf7bd2
Herbie Vine <me@herbievine.com>
2023-11-30 21:48:45 +0100

    chore: remove changes in other projects

--- pipex/env.h
+++ pipex/env.h
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   env.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: herbie <herbie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 17:43:09 by herbie            #+#    #+#             */
-/*   Updated: 2023/10/27 15:39:26 by codespace        ###   ########.fr       */
+/*   Updated: 2023/04/26 17:43:33 by herbie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ENV_H
 # define ENV_H
 
-char	*ft_get_cpy_env(char *key, char **envp);
+char	*ft_get_env(char *key, char **envp);
 char	**ft_get_paths(char **envp);
 
 #endif /* ENV_H */
