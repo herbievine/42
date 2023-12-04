2a6269d140f3d9bcb4a5552cb068ed3ceb206bb7
Herbie Vine <me@herbievine.com>
2023-12-04 14:31:09 +0000

    fix: malloc issue in get home env

--- minishell/env.c
+++ minishell/env.c
@@ -94,15 +94,22 @@ char	**ft_env_get_paths(char **env)
  */
 char	*ft_env_get_home(char **env)
 {
+	char	*home;
+	char	*user;
 	char	*path;
 
-	path = ft_env_get(env, "HOME");
-	if (!path && __MACH__)
-		path = ft_strjoin("/Users/", ft_env_get(env, "USER"));
-	else if (!path)
-		path = ft_strjoin("/home/", ft_env_get(env, "USER"));
-	if (!path)
+	home = ft_env_get(env, "HOME");
+	if (!home)
 		return (NULL);
+	user = ft_env_get(env, "USER");
+	if (!user)
+		return (free(home), NULL);
+	if (__MACH__)
+		path = ft_strjoin("/Users/", ft_env_get(env, "USER"));
+	else
+		path = ft_strjoin("/home/", ft_env_get(env, "USER"));
+	free(home);
+	free(user);
 	return (path);
 }
 
