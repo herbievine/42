b43d629dced4a7743d342685fd0f447f904fd740
Herbie Vine <me@herbievine.com>
2023-12-26 17:16:33 +0100

    fix: segfault when b has no weapon

--- cpp01/ex03/HumanB.cpp
+++ cpp01/ex03/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: herbie <herbie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 15:38:58 by herbie            #+#    #+#             */
-/*   Updated: 2023/12/12 14:06:03 by herbie           ###   ########.fr       */
+/*   Updated: 2023/12/26 17:15:58 by herbie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,10 @@ HumanB::~HumanB()
 
 void HumanB::attack()
 {
-	std::cout << this->name << " attacks with their " << this->weapon->getType() << std::endl;
+	if (!this->weapon)
+		std::cout << this->name << " has no weapon" << std::endl;
+	else
+		std::cout << this->name << " attacks with their " << this->weapon->getType() << std::endl;
 }
 
 void HumanB::setWeapon(Weapon &weapon)
