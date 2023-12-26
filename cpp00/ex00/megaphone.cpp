aa6e6ca3b272e04ab21ec02f016273acabe55699
Herbie Vine <me@herbievine.com>
2023-12-26 10:55:13 +0100

    fix: do megaphone on all args

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,12 +6,20 @@
 /*   By: herbie <herbie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 09:58:21 by herbie            #+#    #+#             */
-/*   Updated: 2023/12/11 10:41:20 by herbie           ###   ########.fr       */
+/*   Updated: 2023/12/26 10:52:36 by herbie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cctype>
+
+void str_toupper(std::string &str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+		str[i] = toupper(str[i]);
+}
 
 int main(int argc, char **argv)
 {
@@ -22,8 +30,12 @@ int main(int argc, char **argv)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
-	for (i = 0; argv[1][i]; i++)
-		std::cout << (char)toupper(argv[1][i]);
+	for (i = 1; i < argc; i++)
+	{
+		std::string str(argv[i]);
+		str_toupper(str);
+		std::cout << str;
+	}
 	std::cout << std::endl;
 	return (0);
 }
