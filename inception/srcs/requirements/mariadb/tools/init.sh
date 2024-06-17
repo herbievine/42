a256c6fef09ca9ef9f8e0ff17c05d65858676601
Herbie Vine <me@herbievine.com>
2024-06-17 07:31:28 +0000

    wip: fuck this is annoying

--- inception/srcs/requirements/mariadb/tools/init.sh
+++ inception/srcs/requirements/mariadb/tools/init.sh
@@ -1,33 +1,63 @@
 #!/bin/sh
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-    echo "initializing mariadb data directory"
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql
-fi
+# # if [ ! -d "/var/lib/mysql/mysql" ]; then
+# 	echo "initializing mariadb data directory"
+# 	mysql_install_db --user=mysql --datadir=/var/lib/mysql
+# # fi
 
-# start mariadb server temporarily in the background
+# # 	mysql -u "$MYSQL_USER" -p"${MYSQL_ROOT_PASSWORD}" -e "\
+# # 		SHOW DATABASES LIKE '${MYSQL_DATABASE}';\
+# # 		CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};\
+# # 		GRANT ALL ON *.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';\
+# # 		FLUSH PRIVILEGES;"
+
+# # # start mariadb server temporarily in the background
+# # mysqld &
+# # pid="$!"
+
+# # # wait for the mariadb server to start
+# # while ! mysqladmin ping --silent; do
+# # 	sleep 1
+# # done
+
+# # # if database does not exist, create it and set up users
+# # if [ -z "$(mysql -u "$MYSQL_USER" -p"${MYSQL_ROOT_PASSWORD}" -e "SHOW DATABASES LIKE '${MYSQL_DATABASE}'" | grep ${MYSQL_DATABASE})" ]; then
+# # 	echo "creating tables..."
+	
+# # 	mysql -u "$MYSQL_USER" -p"${MYSQL_ROOT_PASSWORD}" -e "\
+# # 		SHOW DATABASES LIKE '${MYSQL_DATABASE}';\
+# # 		CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};\
+# # 		GRANT ALL ON *.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';\
+# # 		FLUSH PRIVILEGES;"
+	
+# # 	echo "tables created successfully"
+# # else
+# #   echo "db already exists"
+# # fi
+
+# # # stop the mariadb server
+# # mysqladmin shutdown -p"$pid"
+
+# exec "$@"
+
+
+echo "Initializing MariaDB data directory"
+mysql_install_db --user=mysql --datadir=/var/lib/mysql
+
+echo "Starting MariaDB temporarily to run init.sql"
 mysqld --user=mysql --datadir=/var/lib/mysql --skip-networking &
 pid="$!"
 
-# wait for the mariadb server to start
+# Wait for MariaDB to start
 while ! mysqladmin ping --silent; do
     sleep 1
 done
 
-# if database does not exist, create it and set up users
-if [ -z "$(mysql -u "$MYSQL_USER" -p"${MYSQL_ROOT_PASSWORD}" -e "SHOW DATABASES LIKE '${MYSQL_DATABASE}'" | grep ${MYSQL_DATABASE})" ]; then
-		mysql -u "$MYSQL_USER" -p"${MYSQL_ROOT_PASSWORD}" -e "\
-			SHOW DATABASES LIKE '${MYSQL_DATABASE}';\
-			CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};\
-			GRANT ALL ON *.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';\
-			FLUSH PRIVILEGES;"
-    
-		echo "mariadb is setup"
-else
-    echo "db already exists"
-fi
+# Execute init.sql
+mysql -uroot < /etc/mysql/init.sql
 
-# stop the mariadb server
-mysqladmin shutdown -p"$pid"
+# Shutdown the temporary MariaDB server
+mysqladmin shutdown
 
+# Start MariaDB normally
 exec "$@"
--- inception/srcs/requirements/wordpress/tools/init.sh
+++ inception/srcs/requirements/wordpress/tools/init.sh
@@ -1,23 +1,31 @@
 #!/bin/sh
 
 if [ ! -f "./wp-config.php" ]; then
-	# download wordpress
-	wget http://wordpress.org/latest.tar.gz
-	tar xfz latest.tar.gz
-	mv wordpress/* .
-	rm -rf latest.tar.gz
-	rm -rf wordpress
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	chmod +x wp-cli.phar
+	mv wp-cli.phar /usr/local/bin/wp
 
-	# configure wordpress config
-	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-	cp wp-config-sample.php wp-config.php
+	if php -m | grep -q Phar; then
+	  wp cli version
+		wp core download --allow-root
+
+		# wait a bit to make sure the db is up and running
+		sleep 30
+
+		sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+		sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+		# sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+		sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+		cp wp-config-sample.php wp-config.php
+
+		# wp config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost=localhost --allow-root
+		
+		wp core install --url=hvine.42.fr --title=Inception --admin_user=hvine --admin_password=hvine --admin_email=hvine@student.42.fr --allow-root
+  else
+	  echo "php phar is not installed"
+    exit 1
+	fi
 fi
-
-# wait a bit to make sure the db is up and running
-sleep 10
 
 echo "wordpress is running"
 
