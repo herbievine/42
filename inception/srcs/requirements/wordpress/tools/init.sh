67babe37855f30842286bf9600c8612aa54a0533
Herbie Vine <me@herbievine.com>
2024-06-18 11:49:59 +0000

    feat: nginx + wordpress working

--- inception/srcs/requirements/wordpress/tools/init.sh
+++ inception/srcs/requirements/wordpress/tools/init.sh
@@ -6,19 +6,19 @@ if [ ! -f "./wp-config.php" ]; then
 	mv wp-cli.phar /usr/local/bin/wp
 
 	if php -m | grep -q Phar; then
-	  wp cli version
+	  wp cli version --allow-root
 		wp core download --allow-root
 
 		# wait a bit to make sure the db is up and running
-		sleep 30
+		sleep 10
 
-		sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-		sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-		# sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-		sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-		cp wp-config-sample.php wp-config.php
+		# sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+		# sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+		# # sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+		# sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+		# cp wp-config-sample.php wp-config.php
 
-		# wp config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost=localhost --allow-root
+		wp config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost=mariadb --allow-root
 		
 		wp core install --url=hvine.42.fr --title=Inception --admin_user=hvine --admin_password=hvine --admin_email=hvine@student.42.fr --allow-root
   else
