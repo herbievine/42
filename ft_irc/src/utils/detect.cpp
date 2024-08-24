6578e284913e48a387e4c621c3b9ecdb71a03747
Herbie Vine <me@herbievine.com>
2024-08-24 09:33:28 +0200

    feat: detect either channel or nickname

--- ft_irc/src/utils/detect.cpp
+++ ft_irc/src/utils/detect.cpp
@@ -1,29 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils.hpp                                          :+:      :+:    :+:   */
+/*   detect.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: herbie <herbie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/02 15:05:13 by herbie            #+#    #+#             */
-/*   Updated: 2024/08/24 09:32:58 by herbie           ###   ########.fr       */
+/*   Created: 2024/07/02 15:05:23 by herbie            #+#    #+#             */
+/*   Updated: 2024/08/24 09:32:54 by herbie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef UTILS_HPP
-#define UTILS_HPP
-
+#include "utils.hpp"
 #include <iostream>
 #include <vector>
 
-enum class DetectType
+DetectType detect(std::string channelOrNickname)
 {
-	Channel,
-	Nickname
-};
-
-DetectType detect(std::string channelOrNickname);
-std::vector<std::string> split(std::string str, int sep = ' ');
-void trim(std::string &s);
-
-#endif /* UTILS_HPP */
+	if (channelOrNickname[0] == '#')
+		return DetectType::Channel;
+	return DetectType::Nickname;
+}
