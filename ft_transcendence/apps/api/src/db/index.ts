372b908110b881c345e546f213e361675f13180f
Herbie Vine <me@herbievine.com>
2024-09-16 16:46:24 +0200

    feat: fix db connection

--- ft_transcendence/apps/api/src/db/index.ts
+++ ft_transcendence/apps/api/src/db/index.ts
@@ -3,11 +3,11 @@ import { Client, Connection } from "pg";
 
 export async function getDatabase() {
   const client = new Client({
-    host: "127.0.0.1",
+    host: process.env.POSTGRES_HOST || "127.0.0.1",
     port: 5432,
-    user: "postgres",
-    password: "password",
-    database: "ft_transcendence",
+    user: process.env.POSTGRES_USER || "postgres",
+    password: process.env.POSTGRES_PASSWORD || "password",
+    database: process.env.POSTGRES_DB || "ft_transcendence",
   });
 
   await client.connect();
--- ft_transcendence/apps/api/src/types.ts
+++ ft_transcendence/apps/api/src/types.ts
@@ -1,9 +1,10 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      POSTGRES_USER: string;
-      POSTGRES_PASSWORD: string;
-      POSTGRES_DB: string;
+      POSTGRES_HOST?: string;
+      POSTGRES_USER?: string;
+      POSTGRES_PASSWORD?: string;
+      POSTGRES_DB?: string;
       DB_URL: string;
       INTRA_CLIENT_ID: string;
       INTRA_CLIENT_SECRET: string;
