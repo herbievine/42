7a327310fa2cb949da053d22ce132bfa305c9637
Nicolas Serve <nserve@student.42.fr>
2024-09-26 18:20:59 +0200

    clean model and serializer file from djangoapp

--- ft_transcendence/apps/djangoproject/djangoapp/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from users.models import users
-from tournaments.models import tournaments
-import random
-import string
-
--- ft_transcendence/apps/djangoproject/djangoapp/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import games
-
-class GamesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = games
-        fields = '__all__'
