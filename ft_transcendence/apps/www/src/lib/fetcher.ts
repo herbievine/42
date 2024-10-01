34191a97b0521fd8e8771476f97a1317e058638e
Herbie Vine <me@herbievine.com>
2024-10-01 13:19:34 +0200

    chore: remove logs

--- ft_transcendence/apps/www/src/lib/fetcher.ts
+++ ft_transcendence/apps/www/src/lib/fetcher.ts
@@ -3,7 +3,7 @@ import type { z } from "zod";
 export async function fetcher<S extends z.ZodTypeAny>(
   url: string,
   schema: S,
-  options?: RequestInit,
+  options?: RequestInit
 ): Promise<z.output<S>> {
   return fetch(url, {
     headers: {
@@ -13,26 +13,12 @@ export async function fetcher<S extends z.ZodTypeAny>(
     ...options,
   }).then(async (res) => {
     if (!res.ok) {
-      if (import.meta.env.DEV) {
-        console.error(
-          `An error occurred fetching '${url}'\n${await res.text()}`,
-        );
-      }
-
       throw new Error("An error occurred");
     }
 
     const json = await res.json();
 
     if (schema.safeParse(json).success === false) {
-      if (import.meta.env.DEV) {
-        console.error(
-          `An error occurred validating '${url}'\n${JSON.stringify(json)}\n${
-            schema.safeParse(json).error
-          }`,
-        );
-      }
-
       throw new Error("An error occurred");
     }
 
